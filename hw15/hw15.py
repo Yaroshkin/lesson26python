079f22a9fc7cc59bfd477729f3974e32ed9777ee
Serhii Yaroshkin <yaroshkin255@ukr.net>
2022-12-11 23:04:06 +0200

    add class

---
+++ hw15/hw15.py
@@ -0,0 +1,22 @@
+# Создать классы фигура, прямоугольник, квадрат, круг и эллипс.
+# Реализовать методы вычисления площади
+
+class Figure:
+    def __init__(self):
+        pass
+
+class Rectangle(Figure):
+    def __init__(self):
+        super().__init__()
+
+class Square(Figure):
+    def __init__(self):
+        super().__init__()
+
+class Circle(Figure):
+    def __init__(self):
+        super().__init__()
+
+class Elipse(Figure):
+    def __init__(self):
+        super().__init__()
