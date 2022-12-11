57a834366e86e4ebf9c9f667485f404e2b2c4a6a
Serhii Yaroshkin <yaroshkin255@ukr.net>
2022-12-11 22:40:15 +0200

    lesson15

--- lesson15/main.py
+++ lesson15/main.py
@@ -38,53 +38,103 @@ print(car1.getVin())
 # public, protected, private
 
 class Mlekopit:
-    def __init__(self,age) -> None:
+    def __init__(self, age):
         self.age = age
-    def eatmilk(self):
-        pass
 
+    def eatmilk(self):
+        print('ml eatmilk')
 class Travoyad(Mlekopit):
-    def __init__(self, age, colorGreen):
+    def __init__(self, age, colorGreen, n):
         super().__init__(age)
         self.colorGreen = colorGreen
-        
+        self.n = n
+
     def eatgreen(self):
         print("eat green")
 
+    def eatmilk(self):
+        # return super().eatmilk()
+        print('tr eatmilk')
 class Korova(Travoyad):
-    def __init__(self, age, colorGreen, roga):
-        super().__init__(age, colorGreen)
+    def __init__(self, age, colorGreen, n, roga):
+        super().__init__(age, colorGreen, n)
         self.roga = roga
 
     def eatgreen(self):
+        # print("eat green")
+        print('this cow')
         super().eatgreen()
-        print("this cow")
+
+    def eatmilk(self):
+        # return super().eatmilk()
+        print('cow eatmilk')
 
     def mushanie(self):
         pass
 
+    def __str__(self):
+        # return super().__str__()
+        return f"Cow - {self.age} {self.colorGreen}"
+
+class Koza(Travoyad):
+    def __init__(self, age, colorGreen, n, size):
+        super().__init__(age, colorGreen, n)
+        self.size = size
 
 ml = Mlekopit(12)
-tr = Travoyad(23, 'green')
-cow = Korova(5, 'yellow' , 2)
-# cow = Korova()
+tr = Travoyad(10, 'green', 6)
+cow = Korova(5, 'yellow', 2, 3)
+tr.eatgreen()
 cow.eatgreen()
+koza = Koza(5, 'yellow', 2, 3)
+koza.eatgreen()
+
+cow.eatmilk()
+koza.eatmilk()
+
+li = []
+li.append(ml)
+li.append(tr)
+li.append(cow)
+li.append(koza)
+
+print("##############")
+print(cow)
+
+
+
+for el in li:
+    print('------')
+    # issubclass(Travoyad, el)
+    print(type(el))
+    if isinstance(el, Travoyad):
+        print(type(el).mro())
+        el.eatgreen()
+
+# cow1 = Korova()
+# cow1.eatgreen()
 
 # tr1 = Travoyad()
 
-
 class User:
-    def __init__(self, login, password) -> None:
+    def __init__(self, login='', password='') -> None:
         self.login = login
-        self.password = password
+        self.password = self.__genPassword()
+        self._n = 8
+
+    def __genPassword(self):
+        return 123
 
 class UserProfile(User):
-    def __init__(self, login, password, email, address) -> None:
+    def __init__(self, login='', password='', email='', address='Ukraine') -> None:
         super().__init__(login, password)
         self.email = email
         self.address = address
+        self._n += 1
 
-u = User("asdasd", "asdwqewqr")
+u = User()
 
-
-up = UserProfile("asdf", "asdwqwe", "1@com", "dnipro")
+up = UserProfile(email='email', login='qwertyu')
+print(up.address)
+ 
+#----------------------------------------
