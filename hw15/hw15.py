9ff3fc24b82c00093a14ed0c8e273909ca1ddf3b
yaroshkin <yaroshkin255@ukr.net>
2022-12-12 15:59:01 +0200

    add method in Figure

--- hw15/hw15.py
+++ hw15/hw15.py
@@ -1,13 +1,22 @@
 # Создать классы фигура, прямоугольник, квадрат, круг и эллипс.
 # Реализовать методы вычисления площади
 
+
 class Figure:
-    def __init__(self):
-        pass
+    def __init__(self, name ):
+        self.name = name
+    def areaFigure(self):
+        return
+    def area(self):
+        print(f"{self.name}, площадь {self.areaFigure}")
 
 class Rectangle(Figure):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, side_a, side_b):
+        super().__init__("Rectagle")
+        self.side_a = side_a
+        self.side_b = side_b
+    def areaFigure(self):
+        return self.side_a * self.side_b
 
 class Square(Figure):
     def __init__(self):
@@ -20,3 +29,6 @@ class Circle(Figure):
 class Elipse(Figure):
     def __init__(self):
         super().__init__()
+
+pl = Rectangle(4,5)
+pl.area()
